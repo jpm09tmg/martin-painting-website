d46e893c0e0ca6bb8b363776ca24aa8b6d689cde
Ethan Go <egohobbies@gmail.com>
2025-09-10 02:03:40 -0600

    Update gallery
    added project items

--- app/gallery/page.js
+++ app/gallery/page.js
@@ -5,7 +5,103 @@
     import Footer from '../../components/Footer'
 
     export default function Gallery() {
-        const [images, setImages] = useState([]);
+        //const [images, setImages] = useState([]);
+
+        const [activeFilter, setActiveFilter] = useState('all');
+
+        const projects = [
+            { 
+                id: 1,
+                category: ['exterior', 'residential'],
+                image: '/blueExt.jpg',
+                alt: 'Blue Exterior'
+            },
+            { 
+                id: 2,
+                category: ['interior', 'residential'],
+                image: '/blueLiving.jpg',
+                alt: 'Blue Living Room'
+            },
+            { 
+                id: 3,
+                category: ['interior', 'commercial'],
+                image: '/blueOffice.jpg',
+                alt: 'Blue Office'
+            },
+            { 
+                id: 4,
+                category: ['exterior', 'residential'],
+                image: '/deckstain.jpg',
+                alt: 'Deck Stain'
+            },
+            { 
+                id: 5,
+                category: ['interior', 'residential'],
+                image: 'greenBed.jpg',
+                alt: 'Green Bedroom'
+            },
+            { 
+                id: 6,
+                category: ['interior', 'residential'],
+                image: 'greyBath.jpg',
+                alt: 'Grey Bathroom'
+            },
+            { 
+                id: 7,
+                category: ['interior', 'commercial'],
+                image: 'greyCom.jpg',
+                alt: 'Grey Commercial Hallway'
+            },
+            { 
+                id: 8,
+                category: ['interior', 'commercial'],
+                image: 'greyLiving.jpg',
+                alt: 'Grey Living Room'
+            },
+            { 
+                id: 9,  
+                category: ['interior', 'commercial'],
+                image: 'greyShop.jpg',
+                alt: 'Grey Shop'
+            },
+            { 
+                id: 10,
+                category: ['exterior', 'residential'],
+                image: 'redExt.jpg',
+                alt: 'Red Exterior'
+            },
+            { 
+                id: 11,
+                category: ['interior', 'residential'],
+                image: 'whiteBath.jpg',
+                alt: 'White Bathroom'
+            },
+            { 
+                id: 12,
+                category: ['interior', 'residential'],
+                image: 'whiteKitchen.jpg',
+                alt: 'White Kitchen'
+            },
+            { 
+                id: 13,
+                category: ['interior', 'residential'],
+                image: 'whiteLiving.jpg',
+                alt: 'White Living Room'
+            },
+            { 
+                id: 14,
+                category: ['interior', 'commercial'],
+                image: 'whiteOffice.jpg',
+                alt: 'White Office'
+            },
+            { 
+                id: 15,
+                category: ['exterior', 'residential'],
+                image: 'whiteRedExt.jpg',
+                alt: 'White and Red Exterior'
+            }
+        ]
+
 
         return (
             <>
