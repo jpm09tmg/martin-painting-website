28f19f9a9ca0912ab72915994334828f8d075a1c
Ethan Go <egohobbies@gmail.com>
2025-10-06 23:55:19 -0600

    File organization
    Added folder for project images under public in root.
    Changed admin > (admin) for consistency

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 // app/page.js
 import Link from "next/link";
 import Image from "next/image";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "@/src/components/Header";
+import Footer from "@/src/components/Footer";
 
 export default function Home() {
   // Reusable button styles
