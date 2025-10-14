864056594f6958c80aa2bc9234db742b800a3c17
Ethan Go <egohobbies@gmail.com>
2025-10-14 14:11:54 -0600

    Created API endpoint
    updated route.js for api endpoint

---
+++ src/app/api/chat/route.js
@@ -0,0 +1,15 @@
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
+
+export const maxDuration = 120;
+
+export async function POST(req) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openai("gpt-4o"),
+    messages,
+  });
+
+  return result.toDataStreamResponse();
+}
